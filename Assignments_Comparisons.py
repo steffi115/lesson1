f66f2e1c26b5dc326ee598dc96aae07cd1ea1c3a
steffi115 <chirpychurro@gmail.com>
2024-07-23 21:09:15 -0400

    first commit

---
+++ Assignments_Comparisons.py
@@ -0,0 +1,13 @@
+# Question 5 task 1
+# Value swapping. Swap the values of two given numbers using the assignment operators (=)
+# and then compar them to ensure they have been swapped.\
+
+x = 30
+y = 20
+b = x / y 
+print(b)
+
+x = 20
+y = 30
+b = x / y
+print(b)
