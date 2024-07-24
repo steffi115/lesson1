8c8fbe7506161c2956fe6fb4ff5e21bbe9741700
steffi115 <chirpychurro@gmail.com>
2024-07-23 21:06:58 -0400

    first commit

---
+++ ArithmeticOperations.py
@@ -0,0 +1,21 @@
+#Question 4 task 1
+#Calculate store math. Calculate the total cost of three items you'd commonly find
+# in the grocery store, given their individual prices. For example what would be the cost of
+# bread, peanu butter, and jelly be? Prices don't need to be realistic
+
+bread = 7
+peanutButter = 5
+jelly = 10
+sum = bread + peanutButter + jelly
+print(sum)
+
+#Task 2 Bank interest
+# if you have a savings account with a particular initial amount and a fixed yearly interest rate, 
+# calculate the total amount in your account after a year. So if you had $10,000 at a 7% intest write
+# code that would tell us the amoun at the end of the year. For the example the expected outcome
+# would be $10,700
+
+initial = 10000
+interest = .07
+dividend = initial * interest + initial
+print (dividend)
