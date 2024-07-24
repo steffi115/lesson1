bc335ff481cd696e4719e99dd513aa0c8ed95bba
steffi115 <chirpychurro@gmail.com>
2024-07-23 21:13:29 -0400

    first commit

---
+++ showcase_your_dance_moves.py
@@ -0,0 +1,19 @@
+#Question 1 Task 1
+#Task 1: Code Correction Below is a piece of Python code with incorrect indentation. Your task is to correct it.
+
+weather = "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Task 2 Your mood today. Ask the user how they feel today.
+# If they say "happy, print "That's great to hear!"
+# and if they say "Sad". print "I hope you day gets better!" Ensure your if statement is properly indented
+
+Mood = ("happy")
+if Mood ==  "happy":
+    print("That's great to hear!")
+if Mood == "Sad":
+
+    print("I hope your day gets better!")
