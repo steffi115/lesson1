cb6598b4058a8824418a3e3d85d73bbf7b5cd968
steffi115 <chirpychurro@gmail.com>
2024-07-23 21:12:17 -0400

    first Commit

---
+++ PythonDataTypesAndFunction.py
@@ -0,0 +1,12 @@
+#Question 3 Task 1
+# Givent below are some variable assignments. Your task is to indentify the data type of each
+# variable and then use the type() function to print it out
+
+variable_a = "Hello, World!"
+print(type(variable_a))
+variable_b = 23
+print(type(variable_b))
+variable_c = 3.14
+print(type(variable_c))
+variable_d = True
+print(type(variable_d)) 
